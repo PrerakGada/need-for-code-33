d0657081dded8aef1a5165b7f6798718733113ba
Prerak Gada <prerakgada07@gmail.com>
2022-07-26 23:15:25 +0530

    NNotification Screen Sorted

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../theme.dart';
 import '../widgets/bottom_bar.dart';
 import '../widgets/main_drawer.dart';
+import 'dashboard_screen.dart';
 
 class NotificationsScreen extends StatelessWidget {
   static const String id = 'NotificationScreen';
@@ -10,6 +12,69 @@ class NotificationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Notification');
+    var list = ['PENDING ASSIGNMENTS', 'PASSWORD CHANGED'];
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Card(
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                ),
+                title: Text(list[1]),
+                subtitle: Text('password is changed'),
+                trailing: Text('27-06-2004 12:03pm',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                ),
+                title: Text(list[1]),
+                subtitle: Text('password is changed'),
+                trailing: Text('27-06-2004 12:03pm',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),),
+              ),
+            ),                Card(
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                ),
+                title: Text(list[1]),
+                subtitle: Text('password is changed'),
+                trailing: Text('27-06-2004 12:03pm',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),),
+              ),
+            ),                Card(
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                ),
+                title: Text(list[1]),
+                subtitle: Text('password is changed'),
+                trailing: Text('27-06-2004 12:03pm',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
