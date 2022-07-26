06520c91d238a3babf9f4b4678411af05f39f007
arnitdo <arnav.nitin.deo@gmail.com>
2022-07-26 17:14:19 +0530

    Add user and school models

---
+++ lib/models/schoolModel.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:studybuddy/models/userModel.dart';
+
+class School {
+  late FirebaseFirestore db;
+  late DocumentReference schoolReference;
+
+  School(String schoolId){
+    db = FirebaseFirestore.instance;
+    schoolReference = db.collection("schools").doc(schoolId);
+  }
+
+  Future<Map<String, String?>> getSchoolData() async {
+    Map<String, String?> schoolData = {
+      'schoolAddress' : null,
+      'schoolName' : null,
+      'schoolContact' : null
+    };
+    DocumentSnapshot? document = await schoolReference.get();
+
+    if (document != null){
+      Map<String, dynamic> retrievedData = document.data() as Map<String, dynamic>;
+      schoolData = {
+        'schoolAddress' : retrievedData['school_addr'],
+        'schoolName' : retrievedData['school_name'],
+        'schoolContact' : retrievedData['school_contact']
+      };
+    }
+
+    return schoolData;
+  }
+
+  Future<void> addUser(String userId, String userName, String userAddress, String userContact, String? userRole) async {
+    Map<String, String> userData = <String, String>{
+      'user_address': userAddress,
+      'user_name' : userName,
+      'user_contact' : userContact,
+      'user_role': userRole ?? 'student'
+    };
+
+    DocumentReference newDocReference = schoolReference.collection("users").doc(userId);
+    newDocReference.set(userData, SetOptions(merge: false));
+  }
+}
---
+++ lib/models/userModel.dart
@@ -0,0 +1,56 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:studybuddy/models/schoolModel.dart';
+
+class User {
+  late FirebaseFirestore db;
+  late DocumentReference userReference;
+  late String schoolId;
+
+  User(String schoolID, String userId){
+    schoolId = schoolId;
+    db = FirebaseFirestore.instance;
+    userReference = db.collection("schools").doc(schoolId).collection("users").doc(userId);
+  }
+
+  Future<Map<String, String?>> getUserInfo() async {
+    Map<String, String?> userData = {
+      'userAddress' : null,
+      'userContact' : null,
+      'userName' : null,
+      'userRole' : null
+    };
+
+    DocumentSnapshot? document = await userReference.get();
+
+    if (document != null){
+      Map<String, dynamic> retrievedData = document.data() as Map<String, dynamic>;
+      userData = {
+        'userAddress' : retrievedData['user_address'],
+        'userName' : retrievedData['user_name'],
+        'userContact' : retrievedData['user_contact'],
+        'userRole' : retrievedData['userRole']
+      };
+    }
+
+    return userData;
+  }
+
+  Future<List<String>> getUserClassrooms() async {
+    List<String> userClassrooms = [];
+
+    DocumentSnapshot? document = await userReference.get();
+
+    if (document != null){
+      Map<String, dynamic> retrievedData = document.data() as Map<String, dynamic>;
+      userClassrooms.addAll(retrievedData['user_classrooms']);
+    }
+
+    return userClassrooms;
+  }
+
+  School getUserSchool(userId){
+    return School(schoolId);
+  }
+}
