73989f2899bea3334952ccfd39066ea27a8f2d75
arnitdo <arnav.nitin.deo@gmail.com>
2022-07-26 23:57:17 +0530

    Finish user auth

--- lib/models/schoolModel.dart
+++ lib/models/schoolModel.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class School {
   late FirebaseFirestore db;
   late DocumentReference schoolReference;
+  final FirebaseAuth authInstance = FirebaseAuth.instance;
 
   School(String schoolId) {
     db = FirebaseFirestore.instance;
@@ -28,20 +30,37 @@ class School {
     return schoolData;
   }
 
-  Future<String> addUser(String userName, String userAddress,
-      String userContact, String? userRole) async {
-    Map<String, dynamic> userData = <String, dynamic>{
-      'user_address': userAddress,
-      'user_name': userName,
-      'user_contact': userContact,
-      'user_role': userRole ?? 'student'
-    };
+  Future<void> signupUser(String userId,
+      String userPassword, String userContact, String userAddress,
+      String userName, [String userRole = 'student']) async {
+      final credentials = await authInstance.createUserWithEmailAndPassword(email: userId, password: userPassword);
+      DocumentReference? newUserDocReference = schoolReference.collection(
+          "pending_users").doc(userId);
+      await newUserDocReference.set({
+        "user_name": userName,
+        "user_contact": userContact,
+        "user_address": userAddress,
+        "user_role": userRole
+      });
+    }
 
-    final DocumentSnapshot newUserSnapshot = (await schoolReference
-        .collection("users")
-        .add(userData)) as DocumentSnapshot<Object?>;
-    final String newUserID = newUserSnapshot.id;
-    return newUserID;
+  Future<void> loginUser(String userId, String userPassword) async {
+    final credentials = await authInstance.signInWithEmailAndPassword(email: userId, password: userPassword);
+  }
+
+  Future<void> approveUser(String userId) async {
+    DocumentReference? userDocReference = schoolReference.collection("pending_users").doc(userId);
+    DocumentSnapshot userDocSnapshot = await userDocReference.get();
+
+    DocumentReference? newUserDocReference = schoolReference.collection("users").doc(userId);
+    await newUserDocReference.set({
+      "user_name" : userDocSnapshot["user_name"],
+      "user_contact" : userDocSnapshot["user_contact"],
+      "user_address" : userDocSnapshot["user_address"],
+      "user_role" : userDocSnapshot["user_role"]
+    });
+
+    await userDocReference.delete();
   }
 
   Future<void> updateUser(String userId, String userName, String userAddress,
