b671caafe2d97bcbeaf504c3d0a503af7e633560
Manav <manavshah1104@gmail.com>
2022-07-26 23:05:50 +0530

    Manav code

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -7,7 +7,7 @@ class CalenderScreen extends StatelessWidget {
   static const String id = 'CalenderScreen';
 
   const CalenderScreen({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Text('Calendar');
