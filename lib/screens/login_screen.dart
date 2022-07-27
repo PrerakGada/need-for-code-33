6e3b9fe7c853198648030e3f02b24fe2f097bfb2
Prerak Gada <prerakgada07@gmail.com>
2022-07-27 07:57:57 +0530

    EVERYTHING WORKING

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:studybuddy/screens/home_screen.dart';
 import 'package:studybuddy/theme.dart';
 
 import '../models/utilModel.dart';
+import 'onboarding_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   static const String id = 'LoginScreen';
@@ -179,7 +180,7 @@ class LoginOptions extends StatelessWidget {
                           padding: const EdgeInsets.all(33.0),
                           child: Image(
                             fit: BoxFit.contain,
-                            image: AssetImage('assets/images/ob3.png'),
+                            image: AssetImage('assets/images/teachers.png'),
                             width: MediaQuery.of(context).size.width * 0.2,
                           ),
                         ),
@@ -221,7 +222,7 @@ class LoginOptions extends StatelessWidget {
                           padding: const EdgeInsets.all(33.0),
                           child: Image(
                             fit: BoxFit.contain,
-                            image: AssetImage('assets/images/ob2.png'),
+                            image: AssetImage('assets/images/student.png'),
                             width: MediaQuery.of(context).size.width * 0.2,
                           ),
                         ),
@@ -262,7 +263,7 @@ class LoginOptions extends StatelessWidget {
                           padding: const EdgeInsets.all(33.0),
                           child: Image(
                             fit: BoxFit.contain,
-                            image: AssetImage('assets/images/ob1.png'),
+                            image: AssetImage('assets/images/admin.png'),
                             width: MediaQuery.of(context).size.width * 0.2,
                           ),
                         ),
@@ -371,6 +372,21 @@ class LoginUser extends StatelessWidget {
                         hintText: '333324',
                       ),
                     ),
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) {
+                              return SignupUser(
+                                role: 'student',
+                                id: id,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Text('Signup'),
+                    ),
                   ],
                 ),
               ),
@@ -422,6 +438,189 @@ class LoginUser extends StatelessWidget {
                           TextStyle(color: AppColors.white),
                         ),
                   ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupUser extends StatelessWidget {
+  final String id;
+  final String role;
+
+  const SignupUser({Key? key, required this.id, required this.role})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    late String email = '';
+    late String password = '';
+    late String name = '';
+    late String contact = '';
+    late String address = '';
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: AppColors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Image(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/images/ob1.png'),
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                padding: const EdgeInsets.only(
+                    right: 16, left: 16, top: 24, bottom: 50),
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Enter Your Name',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          name = value;
+                        },
+                        // keyboardType: TextInputType.emailAddress,
+                        // inputFormatters: [FilteringTextInputFormatter.allow()],
+                        decoration: InputDecoration(
+                          hintText: 'Prerak Gada',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Enter Your Contact',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          contact = value;
+                        },
+                        // obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: '333324',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Enter Your Address',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          address = value;
+                        },
+                        // obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: '333324',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Enter Your Email',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        // obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: '333324',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Enter Your Password',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: '333324',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: MaterialButton(
+                height: 42,
+                minWidth: MediaQuery.of(context).size.width * 0.9,
+                color: AppColors.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                onPressed: () async {
+                  var school = School(id);
+                  // var isValid = await school.loginUser(email, password, role);
+                  // if (isValid) {
+                  //   Navigator.of(context).popAndPushNamed(HomeScreen.id);
+                  // } else {
+                  //   print('Invalid creds');
+                  // }
+                  school.signupUser(
+                      email, password, contact, address, name, role);
+                  Navigator.of(context).popAndPushNamed(OnboardingScreen.id);
+                },
+                child: Text(
+                  'Sign Up',
+                  style: Theme.of(context).textTheme.headlineLarge?.merge(
+                        TextStyle(color: AppColors.white),
+                      ),
                 ),
               ),
             ),
